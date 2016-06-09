c35e99b44eecaf206a9ab248a48a92f9aac18cea
Eric Lagarda <eric@infinety.es>
2016-06-09 13:00:45 +0200

    Laravel 5 custom providers package

---
+++ config/providers.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+     |--------------------------------------------------------------------------
+     | Provider Groups
+     |--------------------------------------------------------------------------
+     |
+     | You may configure your environments with provider groups. Providers
+     | and aliases in a provider group will only be loaded in when your
+     | application's environment is present in the group. Effortless.
+     |
+     | You can also set environments to * to load in all environments
+     |
+     */
+    [
+        'environments' => ['local', 'development', 'dev'],
+
+        'providers' => [
+            //
+        ],
+
+        'aliases' => [
+            //
+        ],
+    ]
+
+];
